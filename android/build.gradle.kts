12c7265909980e9c4b465e16ecd613e780bef5ee
Konstantin Unruh <freaxmate@protonmail.com>
2026-05-09 20:53:55 +0200

    update

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -19,6 +19,27 @@ subprojects {
     project.evaluationDependsOn(":app")
 }
 
+// Inject the AGP `namespace` for legacy Flutter plugins that still rely on the
+// (now-removed) `package` attribute in AndroidManifest.xml. Required by
+// isar_flutter_libs 3.1.0+1 (unmaintained) on AGP 8+.
+subprojects {
+    afterEvaluate {
+        if (project.plugins.hasPlugin("com.android.library")) {
+            val androidExt = project.extensions.getByName("android")
+                as com.android.build.gradle.LibraryExtension
+            if (androidExt.namespace == null) {
+                val manifestFile = project.file("src/main/AndroidManifest.xml")
+                if (manifestFile.exists()) {
+                    val match = Regex("package=\"([^\"]+)\"").find(manifestFile.readText())
+                    if (match != null) {
+                        androidExt.namespace = match.groupValues[1]
+                    }
+                }
+            }
+        }
+    }
+}
+
 tasks.register<Delete>("clean") {
     delete(rootProject.layout.buildDirectory)
 }
